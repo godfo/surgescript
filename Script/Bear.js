95d276e0c22a490226bc9c4e28c3cb793a1dcbb1
Dean <ximigami1997@gmail.com>
2020-02-19 16:28:23 +0800

    Update Bear.js

--- Script/Bear.js
+++ Script/Bear.js
@@ -1,11 +1,11 @@
 /*
-Bears笔记
+Bear笔记
 
 [MITM]
 hostname = buy.itunes.apple.com
 
 [Script]
-http-response ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Bear.js,script-update-interval=0
+http-response ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Bear.js,script-update-interval=0
 */
 
 var obj = JSON.parse($response.body);
