50e37d313d462a4e30d5b9ff16d3c219f61e975f
Dean <ximigami1997@gmail.com>
2020-02-19 12:20:38 +0800

    Update and rename Rrtv to Rrtv.js

--- Script/Rrtv.js
+++ Script/Rrtv.js
@@ -1,5 +1,5 @@
 /*
-^https?:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll) url script-response-body https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Rrtv
+^https?:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll) requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Rrtv.js,script-update-interval=0
 */
 
 var body = $response.body;
