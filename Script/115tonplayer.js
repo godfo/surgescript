d8fca0b171924f3ff2062741b2389b0313ad2019
Dean <ximigami1997@gmail.com>
2020-02-19 18:23:57 +0800

    Update 115tonplayer.js

--- Script/115tonplayer.js
+++ Script/115tonplayer.js
@@ -2,7 +2,7 @@
 提取115中的视频使用nPlayer进行播放
 
 [Script]
-http-request ^http?:\/\/.*\.115\.com\/.*\.m3u8.*$ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/115tonplayer.js,script-update-interval=0
+http-request ^http:\/\/.*\.115\.com\/.*\.m3u8.*$ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/115tonplayer.js,script-update-interval=0
 */
 $notification.post('播放地址提取成功, 长按(重按)通知查看', '',  'nplayer-' + $request.url);
 $done({}); 
