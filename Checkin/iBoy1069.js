9f0549802a89cc8bb4bfca838eec764d266c3c6a
Dean <ximigami1997@gmail.com>
2020-03-02 16:39:56 +0800

    Create iBoy1069.js

---
+++ Checkin/iBoy1069.js
@@ -0,0 +1,42 @@
+/*
+iBoy1069.com论坛签到
+
+[Header Rewrite]
+iboy1069.com header-replace Cookie "自己获取的签到的Cookie"
+
+[MITM]
+hostname = iboy1069.com
+
+[Script]
+http-request ^https?:\/\/iboy1069\.com script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Checkin/iBoy1069cookie.js,script-update-interval=0
+cron "0 0 7 * * *" script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Checkin/iBoy1069.js
+
+首先需要当日未签到账户，Surge开启抓取流量，打开iboy1069.com登入，然后进行签到，关闭流量抓取后在已保存的请求查找"qiandao"关键词，找到后选择响应内容为已签到的请求，复制其Cookie添加到header rewrite里，非此签到请求的cookie皆不可，且只能用签到来获取，如果出错就是你Cookie填错了。Header重写之后打开iboy1069.com提示获取Cookie成功就可以删掉重写，并禁用此HTTP-REQUEST script。
+*/
+
+var bonus = {
+  url: 'https://iboy1069.com/plugin.php?id=k_misign:sign&operation=qiandao&format=text&formhash=cd211d82',
+  headers: {
+    Cookie: $persistentStore.read("CookieIB"),
+  }
+};
+$httpClient.get(bonus, function(error, response, data) {
+  if (error) {
+    console.log(error);
+    $notification.post("iBoy1069签到错误", "", "error")
+    $done()
+  } else {
+    if (data.match(/已签到/)) {
+      $notification.post("iBoy1069签到", "", "每日签到成功🎉")
+      $done()
+    } else {
+      if (data.match(/今日已签/)) {
+        $notification.post("iBoy1069签到", "", "签到成功-重复签到 ⚠️")
+        $done()
+       } else {
+         $notification.post("iBoy1069签到", "", "签到失败重新获取Cookie ‼️‼️")
+         $done()
+       }
+     }
+   }
+ })
