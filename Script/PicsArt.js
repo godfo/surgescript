8edef1ea1a92a8cef89cc0005bdee66edb9540b3
Dean <ximigami1997@gmail.com>
2020-03-15 15:32:36 +0800

    修正搬运

--- Script/PicsArt.js
+++ Script/PicsArt.js
@@ -5,7 +5,7 @@ PicsArt/美易 app unlocks pro
 hostname = api.picsart.com, api.meiease.cn
 
 [Script]
-http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/PicsArt.js,script-update-interval=0
 */
 
 let obj = JSON.parse($response.body);
