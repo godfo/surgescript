022224d1d0b8b30231f1824df4b4c0fb95afa100
Dean <ximigami1997@gmail.com>
2020-03-18 11:45:18 +0800

    add lensa修图

--- Script/Lensa.js
+++ Script/Lensa.js
@@ -11,5 +11,5 @@ http-response ^https?:\/\/subscription-service\.neuralprisma\.com\/subscription_
 var obj = JSON.parse($response.body); 
 obj['is_valid'] = true;
 obj['is_trial'] = true;
-obj['expiration_date'] = 2970-01-01T00:00:00Z;
+obj['expiration_date'] = '2970-01-01T00:00:00Z';
 $done({body: JSON.stringify(obj)});
