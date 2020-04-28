f1b4b206a8429d7f070380cab1e92a2c8a1dd2fe
Dean <ximigami1997@gmail.com>
2020-04-28 14:42:44 +0800

    Update Picsew.js

--- Script/Picsew.js
+++ Script/Picsew.js
@@ -11,43 +11,41 @@ http-response ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1
 var obj = JSON.parse($response.body);
 var bundle_id = obj.receipt["bundle_id"];
 if (bundle_id == "com.sugarmo.ScrollClip") {
-  obj = {
-    "receipt": {
+obj = {
+"receipt": {
     "receipt_type": "Production",
-    "app_item_id": 1208145167,
-    "receipt_creation_date": "2019-12-02 07:37:04 Etc/GMT",
+    "adam_id": 0,
+    "app_item_id": 0,
     "bundle_id": "com.sugarmo.ScrollClip",
-    "in_app": [
-      {
-        "product_id": "com.sugarmo.ScrollClip.proUpgrade",
-        "quantity": "1",
-        "transaction_id": "470000537586926",
-        "purchase_date_ms": "1575271039000",
-        "original_purchase_date_pst": "2019-12-01 23:17:19 America/Los_Angeles",
-        "purchase_date_pst": "2019-12-01 23:17:19 America/Los_Angeles",
-        "original_purchase_date_ms": "1575271039000",
-        "is_trial_period": "false",
-        "original_purchase_date": "2019-12-02 07:17:19 Etc/GMT",
-        "original_transaction_id": "470000537586926",
-        "purchase_date": "2019-12-02 07:17:19 Etc/GMT"
-      }
-    ],
-    "download_id": 87052827490254,
-    "adam_id": 1208145167,
-    "receipt_creation_date_pst": "2019-12-01 23:37:04 America/Los_Angeles",
-    "request_date": "2019-12-02 07:37:20 Etc/GMT",
-    "request_date_pst": "2019-12-01 23:37:20 America/Los_Angeles",
-    "version_external_identifier": 833802101,
-    "request_date_ms": "1575272240916",
-    "original_purchase_date_pst": "2019-06-04 21:29:00 America/Los_Angeles",
-    "application_version": "3033",
-    "original_purchase_date_ms": "1559708940000",
-    "receipt_creation_date_ms": "1575272224000",
-    "original_application_version": "2911",
-    "original_purchase_date": "2019-06-05 04:29:00 Etc/GMT"
+    "application_version": "3061",
+    "download_id": 0,
+    "version_external_identifier": 0,
+    "receipt_creation_date": "2020-01-01 00:00:00 Etc/GMT",
+    "receipt_creation_date_ms": "1588000000000",
+    "receipt_creation_date_pst": "2020-04-27 21:10:15 America/Los_Angeles",
+    "request_date": "2020-01-01 00:00:00 Etc/GMT",
+    "request_date_ms": "1588000000000",
+    "request_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+    "original_purchase_date": "2001-01-01 00:00:00 Etc/GMT",
+    "original_purchase_date_ms": "1576200000000",
+    "original_purchase_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+    "original_application_version": "3037",
+    "in_app": [{
+      "quantity": "1",
+      "product_id": "com.sugarmo.ScrollClip.pro",
+      "transaction_id": "1000000000000000",
+      "original_transaction_id": "1000000000000000",
+      "purchase_date": "2020-01-01 00:00:00 Etc/GMT",
+      "purchase_date_ms": "1587700000000",
+      "purchase_date_pst": "2020-01-21 00:00:00 America/Los_Angeles",
+      "original_purchase_date": "2020-01-01 00:00:00 Etc/GMT",
+      "original_purchase_date_ms": "1587700000000",
+      "original_purchase_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+      "is_trial_period": "false"
+    }]
   },
   "status": 0,
   "environment": "Production"
-  };
+ };
 }
 $done({body:JSON.stringify(obj)});
