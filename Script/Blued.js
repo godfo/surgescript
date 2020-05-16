1dcee1a2a7881ca2bccc5d46c21f3a68b6de7a0b
Dean <ximigami1997@gmail.com>
2020-05-16 18:20:00 +0800

    blued闪照获取
    修改自reallinzc 以支持国际版

---
+++ Script/Blued.js
@@ -0,0 +1,17 @@
+/*
+[MITM]
+hostname = *burn-chatfiles.bldimg.com
+
+[Script]
+Blued.js = type=http-request,pattern=https:\/\/.*burn-chatfiles\.bldimg\.com\/,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Blued.js,script-update-interval=0
+*/
+
+var url =$request.url;
+let headers = $request.headers;
+async function launch (){
+    if(headers['User-Agent'].indexOf("Blued")!=-1){
+        $notification.post('捕获到闪照','点击👇URL',url);
+    }
+    $done();
+}
+launch()
