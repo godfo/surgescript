44b900c70fe33910b34291c777039bcbe11cfa4e
Dean <17094934+wubulaba@users.noreply.github.com>
2021-01-04 23:51:36 +0800

    Update PixivPremium.js

--- Script/PixivPremium.js
+++ Script/PixivPremium.js
@@ -2,7 +2,7 @@
 [MITM]
 hostname = oauth.secure.pixiv.net
 [Script]
-PixivPremium = type=http-response,pattern=^https:\/\/oauth\.secure\.pixiv\.net\/auth\/token,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/PixivPremium.js,script-update-interval=0*/
+PixivPremium = type=http-response,pattern=^https:\/\/oauth\.secure\.pixiv\.net\/auth\/token,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/PixivPremium.js,script-update-interval=0
 */
 
 let body = $response.body
