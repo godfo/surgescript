11f539c23739c34718757942413ed20cd1e504bc
Dean <17094934+wubulaba@users.noreply.github.com>
2021-01-05 15:35:01 +0800

    Update Blued.js

--- Script/Blued.js
+++ Script/Blued.js
@@ -10,10 +10,10 @@ var url =$request.url;
 let headers = $request.headers;
 async function launch (){
     if(headers['User-Agent'].indexOf("Blued")!=-1){
-        $notification.post('🐔捕获到闪照❗','👇下滑查看',url);
+        $notification.post('捕获到闪照','下滑通知','点击查看',{url: url});
     }
      if(headers['User-Agent'].indexOf("Media")!=-1){
-        $notification.post('🐔捕获到闪拍❗','👇下滑查看',url);
+        $$notification.post('捕获到闪拍','下滑通知','点击查看',{url: url});
     }
     $done();
 }
