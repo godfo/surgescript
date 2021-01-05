c736b1e0f74aa08a506c6daa43d4f5e9a3150cd1
Dean <17094934+wubulaba@users.noreply.github.com>
2021-01-05 15:40:52 +0800

    Update Blued.js

--- Script/Blued.js
+++ Script/Blued.js
@@ -10,10 +10,10 @@ var url =$request.url;
 let headers = $request.headers;
 async function launch (){
     if(headers['User-Agent'].indexOf("Blued")!=-1){
-        $notification.post('捕获到闪照','下滑通知','点击查看',{url: url});
+        $notification.post('捕获到闪照','点击查看',{url: url});
     }
      if(headers['User-Agent'].indexOf("Media")!=-1){
-        $$notification.post('捕获到闪拍','下滑通知','点击查看',{url: url});
+        $$notification.post('捕获到闪拍','点击查看',{url: url});
     }
     $done();
 }
